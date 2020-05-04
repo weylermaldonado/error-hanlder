d5573f968e9e9559565d5981aaf618d4f7874897
Weyler Maldonado <weyler.maldonado@gmail.com>
2020-05-04 12:48:24 -0500

    docs(*): add doc

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,5 @@
 import ApplicationError from './application.error';
 
-// export {
-//     ApplicationError
-// }
-
- ApplicationError.$config({ debug: true });
-
-
-class TestError extends ApplicationError {
-    constructor(message: string, status: number, code: number) {
-        super(message, status, code);
-    }
+export {
+    ApplicationError
 }
-
-class SecondTestError extends ApplicationError {
-    constructor(message: string, status: number, code: number) {
-        super(message, status, code);
-    }
-}
-
-function log2(this: any) {
-    console.log(`Inside from ${this.constructor.name}`)
-}
-
-SecondTestError.$addEvent(log2)
-
-const a = new SecondTestError('test', 200, 500);
-const b = new TestError('test2', 202, 502);
-
-
-// console.log(a.getError());
